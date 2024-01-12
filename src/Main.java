6757892bd3d5c74226dc001d9cd3fef0ff4b52e6
Jefferson Rodrigues <rodriguesjeff.dev@gmail.com>
2024-01-12 15:27:13 -0300

    Aula 6 - interfaces

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,12 @@
 import aula05.ContaBanco;
+import aula06.ControleRemoto;
 
 public class Main {
     public static void main(String[] args) {
-        ContaBanco c1 = new ContaBanco();
-        c1.setDono("Jecefon");
-        c1.setNumConta(11);
-        c1.abrirConta("CC");
-        c1.depositar(300f);
-
-
-        ContaBanco c2 = new ContaBanco();
-        c2.setDono("Xiquinha");
-        c2.setNumConta(22);
-        c2.abrirConta("CP");
-        c2.depositar(500f);
-        c2.sacar(100);
-        c2.sacar(1000);
-        c2.fecharConta();
-
-
-        c1.estadoAtual();
-        c2.estadoAtual();
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
     }
 }
